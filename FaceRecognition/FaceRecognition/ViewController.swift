5ae314efe6642e3e29330da963805425dbb7db02
Jarrod Servilla <jarrod.servilla@gmail.com>
2019-09-14 15:35:35 -0500

    Uploaded wrong files

---
+++ FaceRecognition/FaceRecognition/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Brian Voong on 7/7/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let image = UIImage(named: "sample2") else { return }
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        
+        let scaledHeight = view.frame.width / image.size.width * image.size.height
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        imageView.backgroundColor = .blue
+        
+        view.addSubview(imageView)
+        
+        let request = VNDetectFaceRectanglesRequest { (req, err) in
+            
+            if let err = err {
+                print("Failed to detect faces:", err)
+                return
+            }
+            
+            req.results?.forEach({ (res) in
+                
+                DispatchQueue.main.async {
+                    guard let faceObservation = res as? VNFaceObservation else { return }
+                    
+                    let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                    
+                    let height = scaledHeight * faceObservation.boundingBox.height
+                    
+                    let y = scaledHeight * (1 -  faceObservation.boundingBox.origin.y) - height
+                    
+                    let width = self.view.frame.width * faceObservation.boundingBox.width
+                    
+                    
+                    let redView = UIView()
+                    redView.backgroundColor = .red
+                    redView.alpha = 0.4
+                    redView.frame = CGRect(x: x, y: y, width: width, height: height)
+                    self.view.addSubview(redView)
+                    
+                    print(faceObservation.boundingBox)
+                }
+            })
+        }
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        DispatchQueue.global(qos: .background).async {
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try handler.perform([request])
+            } catch let reqErr {
+                print("Failed to perform request:", reqErr)
+            }
+        }
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
